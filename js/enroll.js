a8d470f851b87064f44130317c40294dc4d700c3
littledoorboard <mrqul@hotmail.com>
2014-05-06 00:44:04 +0800

    ver_1
    finish reservation and table

--- js/enroll.js
+++ js/enroll.js
@@ -1,6 +1,35 @@
 Parse.initialize("0BribHd5IKm0HZzApohYmyoUmqHCSyu8mwCtFdjq", "Fso20ksEgFz00wQwOyplBP8E5YnSY0fUQqHCkDNo");
 
+function showState(Session, i) {
+	var tag = "#c" + i.toString();
+	var query = new Parse.Query(Session);
+	query.equalTo("tag", "c" + i.toString());
+	query.first({
+		success: function(object) {
+					 var applied = object.get("applied");
+					 if (applied >= 7) {
+						 $(tag).text("已額滿");
+					 }
+					 else {
+						 $(tag).html(" <a href=''>可預約<br>剩餘" + (7-applied).toString() + "位</a> ");
+					 } 
+				 },
+		error: function(error) {
+				   //alert("Error: " + error.code + " " + error.message);
+			   }
+	}); 
+}
+
+function loadTable() {
+	var Session = Parse.Object.extend("Session");
+
+	for (var i=1; i <= 133; ++i) {
+		showState(Session, i);
+	}
+}
+
 $(document).ready(function () {
+	loadTable();
 	$('#submit').click(function (e) {
 		if ($('#name').val() === '') {
 			e.preventDefault();
@@ -8,7 +37,7 @@ $(document).ready(function () {
 		} else if ($('#department').val() === '') {
 			e.preventDefault();
 			alert("請輸入系級");
-		} else if ($('#id').val() === '') {
+		} else if($('#id').val() === '') {
 			e.preventDefault();
 			alert("請輸入學號");
 		} else if ($('#phone').val() === '') {
@@ -42,7 +71,7 @@ $(document).ready(function () {
 								 applicants.push(name);
 								 object.set("applicants", applicants);
 								 object.save();
-								 
+
 								 var Applicant = Parse.Object.extend("Applicant");
 								 var applicant = new Applicant();
 								 applicant.set("date", date);
@@ -52,12 +81,12 @@ $(document).ready(function () {
 								 applicant.set("phone", phone);
 								 applicant.set("isPaid", false);
 								 applicant.save();
-								 
-								 showOnTable(object, applicant);
+
+								 alert('預約成功!\n場次：' + date + ' ' + time + '\n' + name + ' 預約' + amount + '位');
+								 window.location.reload();
 							 } 
-							 else { // 餘額不足
-								 e.preventDefault();
-								 alert('此場次餘額不足');
+							 else { // 票券不足
+								 alert('很抱歉\n此場次票券不足\n請選擇其他場次');
 							 }
 						 },
 					error: function(error) {
@@ -68,11 +97,3 @@ $(document).ready(function () {
 		}
 	});
 });
-
-function showOnTable(session, applicant) {
-	alert('show');
-	var string = "#" + session.get("date") + session.get("time");
-	alert(string);
-	var applicants = session.get("applicants");
-	$('#first_cell').append(applicants[1] + " " + session.get("applied") + "人");
-}
