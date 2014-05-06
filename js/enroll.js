c3cc70420efa976b5ea88b3d15334d0e5a59acf7
littledoorboard <mrqul@hotmail.com>
2014-05-07 02:26:28 +0800

    ver_2.0

--- js/enroll.js
+++ js/enroll.js
@@ -1,5 +1,39 @@
 Parse.initialize("0BribHd5IKm0HZzApohYmyoUmqHCSyu8mwCtFdjq", "Fso20ksEgFz00wQwOyplBP8E5YnSY0fUQqHCkDNo");
-
+/*
+function createSession () {
+	var Session = Parse.Object.extend("Session");
+	for (i=1; i<=7; i++) {
+		var session = new Session();
+		switch (i) {
+			case 1:
+				session.set("date", "5/23");
+				break;
+			case 2:
+				session.set("date", "5/24");
+				break;
+			case 3:
+				session.set("date", "5/25");
+				break;
+			case 4:
+				session.set("date", "5/26");
+				break;
+			case 5:
+				session.set("date", "5/27");
+				break;
+			case 6:
+				session.set("date", "5/28");
+				break;
+			case 7:
+				session.set("date", "5/29");
+				break;
+		}
+		session.set("time", "19:20 - 19:50");
+		session.set("applied", 0);
+		session.set("applicants", []);
+		session.set("tag", "c" + (i+49).toString());
+		session.save();
+	}
+}*/
 function showState(Session, i) {
 	var tag = "#c" + i.toString();
 	var query = new Parse.Query(Session);
@@ -11,7 +45,9 @@ function showState(Session, i) {
 						 $(tag).text("已額滿");
 					 }
 					 else {
-						 $(tag).html(" <a href=''>可預約<br>剩餘" + (7-applied).toString() + "位</a> ");
+						 console.log("1");
+						 var url = "reservation.html?date=" + encodeURIComponent(object.get("date")) + "&time=" + encodeURIComponent(object.get("time"));
+						 $(tag).html("<a href='" + url + "'>剩餘" + (7-applied).toString() + "位</a>");
 					 } 
 				 },
 		error: function(error) {
@@ -23,7 +59,7 @@ function showState(Session, i) {
 function loadTable() {
 	var Session = Parse.Object.extend("Session");
 
-	for (var i=1; i <= 133; ++i) {
+	for (var i=1; i <= 56; ++i) {
 		showState(Session, i);
 	}
 }
@@ -47,13 +83,24 @@ $(document).ready(function () {
 			e.preventDefault();
 			alert("請輸入人數");
 		} else {
+			var queryString = new Array();
+			if (queryString.length === 0) {
+				if (window.location.search.split('?').length > 1) {
+					var params = window.location.search.split('?')[1].split('&');
+					for (var i = 0; i < params.length; i++) {
+						var key = params[i].split('=')[0];
+						var value = decodeURIComponent(params[i].split('=')[1]);
+						queryString[key] = value;
+					}
+				}
+			}
 			var name = $('#name').val();
 			var department = $('#department').val();
 			var id = $('#id').val();
 			var phone = $('#phone').val();
 			var amount = Number($('#amount').val());
-			var date = $('#dateSelect option:selected').text();
-			var time = $('#timeSelect option:selected').text();
+			var date = queryString["date"];
+			var time = queryString["time"];
 			var Session = Parse.Object.extend("Session");
 
 			// 找對應的場次
@@ -83,7 +130,7 @@ $(document).ready(function () {
 								 applicant.save();
 
 								 alert('預約成功!\n場次：' + date + ' ' + time + '\n' + name + ' 預約' + amount + '位');
-								 window.location.reload();
+								 window.location = "index.html";
 							 } 
 							 else { // 票券不足
 								 alert('很抱歉\n此場次票券不足\n請選擇其他場次');
